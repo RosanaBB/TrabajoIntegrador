4bb258f5fb36c6accc446ba9a29b882225aa581c
RosanaBB <119895063+RosanaBB@users.noreply.github.com>
2023-12-25 22:00:35 -0300

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import HomeComponent from './Home';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Our Online Store</h1>
+      <p>
+        Explore our wide range of products and find the best deals. Shop with
+        confidence, and enjoy secure and fast checkout.
+      </p>
+    </div>
+  );
+};
+
+export default HomeComponent;
---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,70 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id);
+
+      if (existingProduct) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  const updateCartItemQuantity = (productId, quantity) => {
+    setCart((prevCart) =>
+      prevCart.map((item) =>
+        item.id === productId ? { ...item, quantity } : item
+      )
+    );
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        updateCartItemQuantity,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  return useContext(CartContext);
+};
+
+export default CartContext;
